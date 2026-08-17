38ff890325f3e42f877bf8b2dfef7441196587fd
A1lonso <alonsobbassini@gmail.com>
2026-08-17 09:46:53 -0300

    Final Results Neo4j Large

--- benchgraph/run_benchmark.sh
+++ benchgraph/run_benchmark.sh
@@ -5,12 +5,12 @@ variants=("research_base_opt" "research_denormalized_opt" "research_intermediate
 for variant in "${variants[@]}"; do
   echo "=== Running variant: $variant ==="
   
-  # Run Neo4j
-  echo "--- Running Neo4j for $variant ---"
+  # Run Memgraph
+  echo "--- Running Memgraph for $variant ---"
   python3 benchmark.py vendor-docker \
-    --vendor-name neo4j-docker \
+    --vendor-name memgraph-docker \
     benchmarks "${variant}/*/*/*" \
-    --export-results "results_${variant}_neo4j_large.json" \
+    --export-results "results_${variant}_memgraph_large.json" \
     --no-authorization \
     --num-workers-for-benchmark 4 \
     --single-threaded-runtime-sec 60 \
